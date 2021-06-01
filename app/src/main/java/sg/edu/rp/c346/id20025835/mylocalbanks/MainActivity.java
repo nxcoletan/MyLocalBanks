dc341546ec9e2221f70795bf87ae3a23fb5ba34c
20025835 <nxcoletan>
2021-06-01 21:24:22 +0800

    commit

--- app/src/main/java/sg/edu/rp/c346/id20025835/mylocalbanks/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20025835/mylocalbanks/MainActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button dbs;
     Button uob;
     Button ocbc;
+
+    String wordClicked = "";
+    String visit="Visit Website";
+    String call= "Contact Bank";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,31 +29,72 @@ public class MainActivity extends AppCompatActivity {
         uob = findViewById(R.id.uob);
         ocbc = findViewById(R.id.ocbc);
 
-        dbs.setOnClickListener((view) -> {
-            Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 1800 + "-" + 111 + "-" + 1111));
-            startActivity(intentCall);
-        });
-
-        uob.setOnClickListener((view) -> {
-            Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 1800 + "-" + 222 + "-" + 2121));
-            startActivity(intentCall);
-        });
-
-        ocbc.setOnClickListener((view) -> {
-            Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 1800 + "-" + 363 + "-" + 3333));
-            startActivity(intentCall);
-        });
+        registerForContextMenu(dbs);
+        registerForContextMenu(uob);
+        registerForContextMenu(ocbc);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v,ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0,0,0,visit);
+        menu.add(0,1,1,call);
+
+        if (v == uob) {
+            wordClicked = "UOB";
+        } else if (v == dbs) {
+            wordClicked = "DBS";
+        } else if(v == ocbc) {
+            wordClicked = "OCBC" ;
+        }
+    }
+        @Override
+        public boolean onContextItemSelected(MenuItem item) {
+            if (wordClicked == "DBS") {
+                if (item.getItemId() == 0) {
+                    Intent intentWebsite = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dbs.com.sg"));
+                    startActivity(intentWebsite);
+                    return true;
+
+                } else if (item.getItemId() == 1) {
+                    Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 1800 + "-" + 111 + "-" + 1111));
+                    startActivity(intentCall);
+                    return true;
+                }
+            } else if (wordClicked == "UOB") {
+                if (item.getItemId() == 0) {
+                    Intent intentWebsite = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uob.com.sg"));
+                    startActivity(intentWebsite);
+                    return true;
+
+                } else if (item.getItemId() == 1) {
+                    Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 1800 + "-" + 222 + "-" + 2121));
+                    startActivity(intentCall);
+                    return true;
+                }
+            } else if (wordClicked == "OCBC") {
+                if (item.getItemId() == 0) {
+                    Intent intentWebsite = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ocbc.com"));
+                    startActivity(intentWebsite);
+                    return true;
+
+                } else if (item.getItemId() == 1) {
+                    Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 1800 + "-" + 363 + "-" + 3333));
+                    startActivity(intentCall);
+                    return true;
+                }
+            }
+            return super.onContextItemSelected(item);
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.EnglishSelection) {
